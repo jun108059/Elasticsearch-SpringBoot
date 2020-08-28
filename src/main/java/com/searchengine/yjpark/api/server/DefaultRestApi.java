af05cf1406455b2b2dc50edf91648451d6b21856
YoungJun Park <youngjun108059@gmail.com>
2020-08-28 16:17:11 +0900

    [Update]🧱bootstrap dependency

--- src/main/java/com/searchengine/yjpark/api/server/DefaultRestApi.java
+++ src/main/java/com/searchengine/yjpark/api/server/DefaultRestApi.java
@@ -2,6 +2,7 @@ package com.searchengine.yjpark.api.server;
 
 import java.time.LocalTime;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +21,40 @@ public class DefaultRestApi {
 		return LocalTime.now().toString();
 	}
 
+}
+
+@Controller
+class WelcomeController {
+	
+	@RequestMapping("/index")
+	public String locale() {
+		return "index";
+	}
+}
+
+@Controller
+class signupController {
+	
+	@RequestMapping("/signup")
+	public String locale() {
+		return "signup";
+	}
+}
+
+@Controller
+class signResultController {
+	
+	@RequestMapping("/signup-result")
+	public String locale() {
+		return "signup-result";
+	}
+}
+
+@Controller
+class registrationController {
+	
+	@RequestMapping("/registration")
+	public String locale() {
+		return "registration";
+	}
 }
