b1a488791efdc03e73259c5cd9dbd5ee15b566d9
YoungJun_Park <youngjun108059@gmail.com>
2020-08-31 14:21:14 +0900

    [Update]✍MVC 템플릿 테스트

--- src/main/java/com/searchengine/yjpark/controller/HelloController.java
+++ src/main/java/com/searchengine/yjpark/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.searchengine.yjpark.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "YoungJun!");
         return "hello"; // Templates 으로 렌더링
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
