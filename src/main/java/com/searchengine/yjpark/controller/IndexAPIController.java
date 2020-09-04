cef34b32189b382caefc8464a355e4925522c25a
YoungJun Park <youngjun108059@gmail.com>
2020-09-04 18:08:52 +0900

    [Update]💡Elasticsearch - Java Rest Client 활용 - Create index

--- src/main/java/com/searchengine/yjpark/controller/IndexAPIController.java
+++ src/main/java/com/searchengine/yjpark/controller/IndexAPIController.java
@@ -1,5 +1,16 @@
 package com.searchengine.yjpark.controller;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,89 +38,85 @@ public class IndexAPIController {
                 RestHighLevelClient client = createConnection()
         ){
             //index name
-            String indexName = "sample-korean-index";
+            String indexName = "saramin";
             //type name
-            String typeName = "_doc";
+            String typeName = "curation";
 
             //settings
-            XContentBuilder settingsBuilder = XContentFactory.jsonBuilder()
-                    .startObject()
-                    .field("number_of_shards",5)
-                    .field("number_of_replicas",1)
-
-                    .startObject("analysis")
-                    .startObject("tokenizer")
-                    .startObject("sample-nori-tokenizer")
-                    .field("type","nori_tokenizer")
-                    .field("decompound_mode","mixed")
-                    .field("user_dictionary","user_dictionary.txt")
-                    .endObject()
-                    .endObject()
-
-                    .startObject("analyzer")
-                    .startObject("sample-nori-analyzer")
-                    .field("type","custom")
-                    .field("tokenizer","sample-nori-tokenizer")
-                    .array("filter",new String[]{
-                                    "sample-nori-posfilter",
-                                    "nori_readingform",
-                                    "sample-synonym-filter",
-                                    "sample-stop-filter"
-                            }
-                    )
-                    .endObject()
-                    .endObject()
-
-                    .startObject("filter")
-                    .startObject("sample-nori-posfilter")
-                    .field("type","nori_part_of_speech")
-                    .array("stoptaags",new String[] {
-                                    "E","IC","J","MAG","MM","NA","NR","SC",
-                                    "SE","SF","SH","SL","SN","SP","SSC","SSO",
-                                    "SY","UNA","UNKNOWN","VA","VCN","VCP","VSV",
-                                    "VV","VX","XPN","XR","XSA","XSN","XSV"
-                            }
-                    )
-                    .endObject()
-
-                    .startObject("sample-synonym-filter")
-                    .field("type","synonym")
-                    .field("synonyms_path","synonymsFilter.txt")
-                    .endObject()
-
-                    .startObject("sample-stop-filter")
-                    .field("type","stop")
-                    .field("stopwords_path","stopFilter.txt")
-                    .endObject()
-                    .endObject()
-                    .endObject()
-                    .endObject();
+            XContentBuilder settingsBuilder = XContentFactory.jsonBuilder();
+            settingsBuilder.startObject();
+            {
+                settingsBuilder.field("number_of_shards",5);
+                settingsBuilder.field("number_of_replicas",1);
+            }
+            settingsBuilder.endObject();
 
             //mapping info
-            XContentBuilder indexBuilder = XContentFactory.jsonBuilder()
-                    .startObject()
-                    .startObject(typeName)
+            XContentBuilder indexBuilder = XContentFactory.jsonBuilder();
+            indexBuilder.startObject();
+            {
+                indexBuilder.startObject(typeName)
                     .startObject("properties")
-                    .startObject("question")
-                    .field("type","text")
-                    .field("analyzer","sample-nori-analyzer")
+                        .startObject("curation_seq")
+                            .field("type", "integer")
+                        .endObject()
+                        .startObject("curation_title")
+                            .field("type","text")
+                        .endObject()
+                        .startObject("template")
+                           .field("type","text")
+                        .endObject()
+                        .startObject("template_img")
+                            .field("type","text")
+                        .endObject()
+                        .startObject("ending_txt")
+                            .field("type","text")
+                        .endObject()
+                        .startObject("tag")
+                            .field("type","text")
+                        .endObject()
+                        .startObject("pc_hits")
+                            .field("type","integer")
+                        .endObject()
+                        .startObject("m_hits")
+                            .field("type","integer")
+                        .endObject()
+                        .startObject("reservation_dt")
+                            .field("type","date")
+                        .endObject()
+                        .startObject("reg_dt")
+                            .field("type","date")
+                        .endObject()
+                        .startObject("up_dt")
+                            .field("type","date")
+                        .endObject()
+                        .startObject("status")
+                            .field("type","text")
+                        .endObject()
+                        .startObject("open_fl")
+                            .field("type","text")
+                        .endObject()
+                        .startObject("main_fl")
+                            .field("type","text")
+                        .endObject()
+                        .startObject("writer")
+                            .field("type","text")
+                        .endObject()
                     .endObject()
-                    .startObject("answer")
-                    .field("type","keyword")
-                    .endObject()
-                    .endObject()
-                    .endObject()
-                    .endObject();
+                .endObject();
+            }
+            indexBuilder.endObject();
 
             //인덱스생성 요청 객체
             CreateIndexRequest request = new CreateIndexRequest(indexName);
             //세팅 정보
             request.settings(settingsBuilder);
             //매핑 정보
-            request.mapping(typeName, indexBuilder);
+            request.mapping(indexBuilder); //어떤 type인지 명시를 안했는데...
+
 
             //별칭설정
-            String aliasName = "chatbotInstance";
+            String aliasName = "curationType";
             request.alias(new Alias(aliasName));
 
             //인덱스생성
@@ -117,11 +124,12 @@ public class IndexAPIController {
 
             acknowledged = response.isAcknowledged();
 
+
+
         }catch (Exception e) {
             e.printStackTrace();
             return "인덱스 생성에 실패하였습니다. - catch";
         }
-
 
         return acknowledged == true ? "인덱스가 생성되었습니다.":"인덱스생성에 실패하였습니다.";
     }
@@ -136,14 +144,12 @@ public class IndexAPIController {
         ){
 
             //index name
-            String indexName = "sample-korean-index";
+            String indexName = "saramin";
 
             //인덱스 삭제 요청 객체
             DeleteIndexRequest request = new DeleteIndexRequest(indexName);
-
-            DeleteIndexResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
-
-            acknowledged = response.isAcknowledged();
+            AcknowledgedResponse deleteIndexResponse = client.indices().delete(request, RequestOptions.DEFAULT);
+            acknowledged = deleteIndexResponse.isAcknowledged();
         }catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
@@ -151,16 +157,5 @@ public class IndexAPIController {
         }
 
         return acknowledged == true ? "인덱스 삭제가 완료되었습니다.":"인덱스 삭제에 실패하였습니다.";
-    }
-
-    /*
-     * connection create method
-     */
-    public RestHighLevelClient createConnection() {
-        return new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("127.0.0.1",9200,"http")
-                )
-        );
     }
 }
