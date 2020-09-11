b731805ac3f156a5e75ef2335493f7fbda5d2903
YoungJun Park <youngjun108059@gmail.com>
2020-09-11 18:00:47 +0900

    [Update]🕹API 컨트롤러 - RequestBody Mapping Test

--- src/main/java/com/searchengine/yjpark/api/controller/APIController.java
+++ src/main/java/com/searchengine/yjpark/api/controller/APIController.java
@@ -1,52 +1,62 @@
 
 package com.searchengine.yjpark.api.controller;
 
-import com.searchengine.yjpark.domain.DataBaseInfo;
-import com.searchengine.yjpark.domain.Service;
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.admin.indices.alias.Alias;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import com.searchengine.yjpark.api.response.SearchResponse;
+import com.searchengine.yjpark.api.service.IndexService;
+import com.searchengine.yjpark.domain.Indexing;
+import com.searchengine.yjpark.domain.Search;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
 
 @RestController
 @RequestMapping("/simple/search")
 public class APIController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     // 크게 검색 & 색인
     @Autowired
     RestHighLevelClient restHighLevelClient;
 
-    @RequestMapping("/bulk")
-    @GetMapping("args")
-    public void bulkIndex(@RequestParam(value = "id")String id) {
-        // Index Request
-        String indexId = "myIndex_" + id; // prefix 설정
-        IndexRequest request = new IndexRequest(indexId);
-        request.id(indexId); // index id 할당
+    @Autowired
+    IndexService indexService;
 
+    @GetMapping("/bulk")
+    public void bulkIndex(@RequestParam(value = "service_id")String serviceId) {
 
+        log.info("Bulk Get : {}", serviceId);
+
+        IndexService indexService;
 
     }
+
+    // simple/search/contents/{serviceId}/{keyWord}
+    @PostMapping("/contents")
+    public void searchKeyword(@RequestBody Search search) {
+
+        log.info("Search POST Test : {}", search.toString());
+    }
+
+    @PostMapping("/indexing/id")
+    public void indexingDocument(@RequestBody Indexing indexing) {
+
+        log.info("Search POST Test : {}", indexing.toString());
+    }
+
+    @GetMapping("searchResponse")
+    public SearchResponse getSearchResponse() {
+        SearchResponse searchResponse = new SearchResponse();
+        searchResponse.setTotal(1233);
+
+        indexService.testIsIndexExist();
+
+        return searchResponse;
+    }
+
+
 
 }
 
