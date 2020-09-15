3145267a49c4c89501864ae379cf22c256c169d6
YoungJun Park <youngjun108059@gmail.com>
2020-09-15 21:16:37 +0900

    [Update]🕹Contents 부분 색인 로직 수정

--- src/main/java/com/searchengine/yjpark/api/controller/APIController.java
+++ src/main/java/com/searchengine/yjpark/api/controller/APIController.java
@@ -1,10 +1,11 @@
 
 package com.searchengine.yjpark.api.controller;
 
-import com.searchengine.yjpark.api.response.SearchResponse;
+import com.searchengine.yjpark.api.response.SearchResultResponse;
 import com.searchengine.yjpark.api.service.IndexService;
 import com.searchengine.yjpark.domain.Indexing;
 import com.searchengine.yjpark.domain.Search;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,39 +25,60 @@ public class APIController {
     IndexService indexService;
 
     @GetMapping("/bulk")
-    public void bulkIndex(@RequestParam(value = "service_id")String serviceId) {
+    public String bulkIndex(@RequestParam(value = "service_id")String serviceId) {
         log.info("Bulk Get : {}", serviceId);
         // 서비스 로직 호출
-        indexService.bulkIndex(serviceId);
-        // Todo 컨트롤러에서 Response 반환할 필요가 있는지
+        boolean bulkSuccess = indexService.bulkIndex(serviceId);
+        log.info("bulkSuccess : {}", bulkSuccess);
+        if(bulkSuccess) {
+            return "/simple/viewBulkIndex";
+        }
+        else {
+           return "/simple/error";
+        }
     }
 
     // simple/search/contents/{serviceId}/{keyWord}
     @PostMapping("/contents")
-    public void searchKeyword(@RequestBody Search search) {
+    public SearchResultResponse searchKeyword(@RequestBody Search search) {
         // 클라이언트가 {서비스ID} + {검색단어} + {...} 선택 -> 검색 or JSON POST
-        log.info("Search POST Test : {}", search.toString());
+        log.info("> Search POST Test : {}", search.toString());
         // 서비스 로직 호출
-        indexService.searchKeyword(search);
+        SearchResultResponse searchResultResponse = new SearchResultResponse();
+        return indexService.searchKeyword(search);
     }
 
     @GetMapping("searchResponse")
-    public SearchResponse getSearchResponse() {
-        SearchResponse searchResponse = new SearchResponse();
-        searchResponse.setTotal(1233);
-
+    public SearchResultResponse getSearchResponse() {
+        SearchResultResponse searchResultResponse = new SearchResultResponse();
+        searchResultResponse.setTotal(1233);
         indexService.testIsIndexExist();
 
-        return searchResponse;
+        return searchResultResponse;
     }
 
     @PostMapping("/indexing/id")
-    public void indexingDocument(@RequestBody Indexing indexing) {
-        // 컨텐츠 부분 색인
+    public void indexingCreateDocument(@RequestBody Indexing indexing) {
+        // 컨텐츠 부분 색인 (생성)
         // indexing > 서비스ID, 연산 타입, 컨텐츠 ID
-        // Todo 컨텐츠 id == 도큐먼트 ID?
-//        indexService
         log.info("Search POST Test : {}", indexing.toString());
+        indexService.createDocument(indexing);
+    }
+
+    @PutMapping("/indexing/id")
+    public void indexingUpdateDocument(@RequestBody Indexing indexing) {
+        // 컨텐츠 부분 색인 (수정)
+        // indexing > 서비스ID, 연산 타입, 컨텐츠 ID
+        log.info("Search POST Test : {}", indexing.toString());
+        indexService.updateDocument(indexing);
+    }
+
+    @DeleteMapping("/indexing/id")
+    public void indexingDeleteDocument(@RequestBody Indexing indexing) {
+        // 컨텐츠 부분 색인 (삭제)
+        // indexing > 서비스ID, 연산 타입, 컨텐츠 ID
+        log.info("Search POST Test : {}", indexing.toString());
+        indexService.deleteDocument(indexing);
     }
 }
 
