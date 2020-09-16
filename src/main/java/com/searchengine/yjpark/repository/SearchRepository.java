c48a21b058fa940a863c9407d81b26ec8e6fdc2a
YoungJun Park <youngjun108059@gmail.com>
2020-09-16 16:26:53 +0900

    [Refact]♻불필요 코드 제거

--- src/main/java/com/searchengine/yjpark/repository/SearchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.searchengine.yjpark.repository;
-
-import com.searchengine.yjpark.domain.DataBaseInfo;
-
-import java.util.List;
-
-public interface SearchRepository {
-    int countDocument(List<DataBaseInfo> dbInfo, com.searchengine.yjpark.domain.Service serviceInfo);
-}
